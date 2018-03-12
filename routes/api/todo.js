989c56afaae25296ea546c33416398ca1335cfc3
sanjay555 <sanjayshr555@gmail.com>
2018-03-12 23:56:00 +0530

    CRUD API is Done

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -14,7 +14,7 @@ router.get('/todos', (req, res, next) => {
 });
 
 // Single Todo
-router.get('/todo/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   const id = req.params.id;
   Todo.findById(id,(err, todo) => {
     if(err) {
@@ -42,6 +42,39 @@ router.post('/new', (req, res, next) => {
   }
 })
 
-// 
+// Update
+router.put('/:id', (req, res, next) => {
+  let todo = req.body;
+  const id = req.params.id;  
+  let updateTodo = {};  
+  if (todo.title) {
+    updateTodo.title = todo.title;   
+  }
+  if(!updateTodo) {
+    res.status(400);
+    res.json({ "error":"Bad data"});
+  } else {
+    Todo.findByIdAndUpdate(id, updateTodo,{new: true},  (err, todo) => {
+      if(err) {
+        res.send(err)
+      } else {        
+        res.json(todo)
+      }
+    });
+  }
+})
+
+
+// Delete
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id;  
+  Todo.findByIdAndRemove(id, (err) => {
+    if (err) {
+      res.send(err)
+    } else {
+    res.json({ message: "Successfully deleted" });
+    }
+  });
+});
 
 module.exports = router;
